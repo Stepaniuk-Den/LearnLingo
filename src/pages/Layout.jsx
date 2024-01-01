fdd1b6e0debbf91414fcdd5fef21e11a80bc5f7a
Stepaniuk-Den <stepaniuk.den@gmail.com>
2024-01-01 21:55:23 +0200

    update

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1,12 +1,13 @@
 import { Outlet } from "react-router-dom";
-import React from 'react'
+import React from "react";
+import { StyledContainer } from "./Layout.styled";
 
 const Layout = () => {
   return (
-    <>
-    <Outlet/>
-    </>
-  )
-}
+    <StyledContainer>
+      <Outlet />
+    </StyledContainer>
+  );
+};
 
 export default Layout;
---
+++ src/pages/Layout.styled.js
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+
+export const StyledContainer = styled.div`
+  width: 1440px;
+  padding: 0 64px 32px;
+`;
