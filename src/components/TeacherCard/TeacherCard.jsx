c78482661b506b1e7f5e74fa2be5bfb84dbf13e2
Stepaniuk-Den <stepaniuk.den@gmail.com>
2024-01-22 22:08:31 +0200

    update

--- src/components/TeacherCard/TeacherCard.jsx
+++ src/components/TeacherCard/TeacherCard.jsx
@@ -13,6 +13,15 @@ const TeacherCard = ({ teacher }) => {
         <img src={AvatarImg} alt="avatar_default" loading="lazy" />
         <StyledIsOnline />
       </ImageTeacherWrapper>
+      <div>
+        <div>
+          <div>
+            <p>Languages</p>
+            <p>Jane Smith</p>
+          </div>
+        </div>
+        <div></div>
+      </div>
     </StyledCardWrapper>
   );
 };
